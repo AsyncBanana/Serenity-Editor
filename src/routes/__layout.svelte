6550500066e0a75e9aa899298ca7b566f6b3c96d
AsyncBanana <jacobedwardjackson@outlook.com>
2021-07-18 08:43:28 -0400

    still trying to fix sw

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,4 @@
 <script>
   import "virtual:windi.css";
-  import PWA from "$lib/components/PWA.svelte";
-  import {browser} from '$app/env'
 </script>
-{#if browser}
-   <PWA></PWA>
-{/if}
 <slot></slot>
